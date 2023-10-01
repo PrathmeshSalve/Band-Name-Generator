06d16b711b3e3261fcc2415c39faf1915f7a031d
Prathmesh Salve <116294312+PrathmeshSalve@users.noreply.github.com>
2023-10-01 22:51:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+LEVEL : B E G I N N E R.
+----------------------------------------------------------
+Project - Prints the band name by taking input from user.
+----------------------------------------------------------
+Functions Used:
+* Print - Prints a string into the console. ex: print("Hello World").
+* Input - Prints a string into the console, and asks the user for a string input. || ex: name = input("What's your name").
+"""
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be: "+city+" "+pet+".")
